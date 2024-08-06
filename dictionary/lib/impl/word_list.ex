698266c7b4a9e9f571a11d87df548b11b20ae2be
Nick Olson <naolson.1819@gmail.com>
2024-08-06 10:00:39 -0500

    added genserver to hangman

--- dictionary/lib/impl/word_list.ex
+++ dictionary/lib/impl/word_list.ex
@@ -3,12 +3,13 @@ defmodule Dictionary.Impl.WordList do
 
   @spec word_list() :: t
   def word_list do
-    "./assets/words.txt"
-      |> File.read!()
-      |> String.split(~r/\n/, trim: true)
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/, trim: true)
   end
 
-  @spec random_word(t) :: String.t
+  @spec random_word(t) :: String.t()
   def random_word(word_list) do
     word_list
     |> Enum.random()
