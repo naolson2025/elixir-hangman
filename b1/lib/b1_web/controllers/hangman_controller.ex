75dd272e8c26b7cfc6d66f4e8ab1f2cef6d1a302
Nick Olson <naolson.1819@gmail.com>
2024-09-09 09:46:19 -0500

    stuff

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -9,18 +9,28 @@ defmodule B1Web.HangmanController do
 
   def new(conn, _params) do
     game = Hangman.new_game()
-    tally = Hangman.tally(game)
+
     conn
     |> put_session(:game, game)
-    |> render("game.html", tally: tally)
+    |> redirect(to: ~p"/hangman/current")
   end
 
   def update(conn, params) do
     guess = params["make_move"]["guess"]
+
+    conn
+    |> get_session(:game)
+    |> Hangman.make_move(guess)
+
+    redirect(conn, to: ~p"/hangman/current")
+  end
+
+  def show(conn, _params) do
     tally =
       conn
       |> get_session(:game)
-      |> Hangman.make_move(guess)
+      |> Hangman.tally()
+
     render(conn, "game.html", tally: tally)
   end
 end
