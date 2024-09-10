376a59a22535c36255127993e07acd3a3c1e52d8
Nick Olson <naolson.1819@gmail.com>
2024-09-10 09:52:29 -0500

    finished client

--- b1/lib/b1_web/controllers/hangman_html.ex
+++ b1/lib/b1_web/controllers/hangman_html.ex
@@ -8,6 +8,45 @@ defmodule B1Web.HangmanHTML do
 
   embed_templates "hangman_html/*"
 
+  @status_field %{
+    initializing: {"initializing", "Guess the word, one letter at a time"},
+    good_guess: {"good-guess", "Good guess!"},
+    bad_guess: {"bad-guess", "Bad guess!"},
+    won: {"won", "You won"},
+    lost: {"lost", "You lost"},
+    already_used: {"already-used", "Already used that letter"}
+  }
+  def move_status(status) do
+    {class, msg} = @status_field[status]
+    "<div class='status #{class}'>#{msg}</div>"
+  end
+
+  attr :state, :atom, default: :initializing
+  # it's not always required, but it's helpful to annotate your function components
+  # with these attr/3 calls. More info: https://hexdocs.pm/phoenix_live_view/Phoenix.Component.html#attr/3
+
+  def continue_or_try_again(assigns) do
+    # within an elixir file, you begin a block of HEEx code using the ~H sigil.
+    # functions that return HEEX must take exactly one param -- an `assigns` map
+    # any key from `assigns` can be referenced using @ syntax
+    ~H"""
+    <.link :if={@state in [:won, :lost]} href={~p"/hangman"}>
+      <.button>Try again</.button>
+    </.link>
+    <.form
+      :let={f}
+      :if={@state not in [:won, :lost]}
+      for={%{}}
+      as={:make_move}
+      action={~p"/hangman"}
+      method="put"
+    >
+      <.input field={f[:guess]} />
+      <.button>Make next guess</.button>
+    </.form>
+    """
+  end
+
   def figure_for(0) do
     ~s{
       ┌───┐
--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -1,6 +1,10 @@
 defmodule Hangman.Runtime.Server do
   @type t :: pid
+  @idle_timeout 1 * 60 * 60 * 1000   # 1 hour
+
   alias Impl.Game
+  alias Hangman.Runtime.Watchdog
+
   use GenServer
 
   ##### Client Process
@@ -11,16 +15,19 @@ defmodule Hangman.Runtime.Server do
 
   ##### Server Process
 
-  def init(_init_arg) do
-    {:ok, Game.new_game()}
+  def init(_) do
+    watcher = Watchdog.start(@idle_timeout)
+    { :ok, { Game.new_game, watcher }}
   end
 
-  def handle_call({:make_move, guess}, _from, game) do
-    {updated_game, tally} = Game.make_move(game, guess)
-    {:reply, tally, updated_game}
+  def handle_call({ :make_move, guess }, _from, { game, watcher }) do
+    Watchdog.im_alive(watcher)
+    { updated_game, tally } = Game.make_move(game, guess)
+    { :reply, tally, { updated_game, watcher } }
   end
 
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game}
+  def handle_call({ :tally }, _from, { game, watcher }) do
+    Watchdog.im_alive(watcher)
+    { :reply, Game.tally(game), { game, watcher } }
   end
 end
